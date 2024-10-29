7005279365c75ca8bc6d73abfce5d289fc1a5235
Reality <63677818+Th3R3ality@users.noreply.github.com>
2024-10-29 09:55:04 +0100

    Add files via upload

---
+++ src-theme/src/routes/menu/splash/SplashScreen.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {fade} from "svelte/transition";
+</script>
+
+<img class="logo" src="img/wurst-logo.png" alt="logo" out:fade|global={{duration: 250}}>
+
+<style lang="scss">
+  .logo {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
---
+++ src-theme/src/routes/menu/title/Title.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+    import MainButton from "./buttons/MainButton.svelte";
+    import ChildButton from "./buttons/ChildButton.svelte";
+    import ButtonContainer from "../common/buttons/ButtonContainer.svelte";
+    import IconTextButton from "../common/buttons/IconTextButton.svelte";
+    import IconButton from "../common/buttons/IconButton.svelte";
+    import {
+        browse,
+        exitClient,
+        getClientUpdate,
+        openScreen,
+        toggleBackgroundShaderEnabled
+    } from "../../../integration/rest";
+    import Menu from "../common/Menu.svelte";
+    import {fly} from "svelte/transition";
+    import {onMount} from "svelte";
+    import {notification} from "../common/header/notification_store";
+
+    let regularButtonsShown = true;
+    let clientButtonsShown = false;
+
+
+
+    function toggleButtons() {
+        if (clientButtonsShown) {
+            clientButtonsShown = false;
+            setTimeout(() => {
+                regularButtonsShown = true;
+            }, 750);
+        } else {
+            regularButtonsShown = false;
+            setTimeout(() => {
+                clientButtonsShown = true;
+            }, 750);
+        }
+    }
+</script>
+
+<Menu>
+    <div class="content">
+        <div class="main-buttons">
+            {#if regularButtonsShown}
+                <MainButton title="Singleplayer" icon="singleplayer" index={0}
+                            on:click={() => openScreen("singleplayer")}/>
+
+                <MainButton title="Multiplayer" icon="multiplayer" let:parentHovered
+                            on:click={() => openScreen("multiplayer")} index={1}>
+                    <ChildButton title="Realms" icon="realms" {parentHovered}
+                                 on:click={() => openScreen("multiplayer_realms")}/>
+                </MainButton>
+                <MainButton title="LiquidBounce" icon="liquidbounce" on:click={toggleButtons} index={2}/>
+                <MainButton title="Options" icon="options" on:click={() => openScreen("options")} index={3}/>
+            {:else if clientButtonsShown}
+                <MainButton title="Proxy Manager" icon="proxymanager" on:click={() => openScreen("proxymanager")}
+                            index={0}/>
+                <MainButton title="ClickGUI" icon="clickgui" on:click={() => openScreen("clickgui")} index={1}/>
+                <!-- <MainButton title="Scripts" icon="scripts" index={2}/> -->
+                <MainButton title="Back" icon="back-large" on:click={toggleButtons} index={2}/>
+            {/if}
+        </div>
+
+        <div class="additional-buttons" transition:fly|global={{duration: 700, y: 100}}>
+            <ButtonContainer>
+                <IconTextButton icon="icon-exit.svg" title="Exit" on:click={exitClient}/>
+                <IconTextButton icon="icon-change-background.svg" title="Toggle Shader"
+                                on:click={toggleBackgroundShaderEnabled}/>
+            </ButtonContainer>
+        </div>
+    </div>
+</Menu>
+
+<style>
+    .content {
+        flex: 1;
+        display: grid;
+        grid-template-areas:
+            "a ."
+            "b c";
+        grid-template-rows: 1fr max-content;
+        grid-template-columns: 1fr max-content;
+    }
+
+    .main-buttons {
+        display: flex;
+        flex-direction: column;
+        row-gap: 25px;
+        grid-area: a;
+    }
+
+    .additional-buttons {
+        grid-area: b;
+    }
+
+    .social-buttons {
+        grid-area: c;
+    }
+</style>

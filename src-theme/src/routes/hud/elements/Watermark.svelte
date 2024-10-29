52cf5a023c30426082f0687b5eadb4d85649406d
Reality <63677818+Th3R3ality@users.noreply.github.com>
2024-10-29 09:54:23 +0100

    Add files via upload

---
+++ src-theme/src/routes/hud/elements/Watermark.svelte
@@ -0,0 +1,10 @@
+<img class="watermark" src="img/wurst-logo.png" alt="watermark" />
+
+<style lang="scss">
+    .watermark {
+        //position: fixed;
+        //top: 15px;
+        //left: 15px;
+        width: 165px;
+    }
+</style>
---
+++ src-theme/src/routes/hud/elements/notifications/Notification.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let title: string;
+    export let message: string;
+    export let severity: string;
+</script>
+
+<div class="notification">
+    <div class="title">{title}</div>
+    <div class="message">{message}</div>
+</div>
+
+<style lang="scss">
+  @import "../../../../colors.scss";
+
+  .notification {
+    display: flex;
+    flex-flow: row wrap;
+    border-left: 2px solid $accent-color;
+    column-gap: 10px;
+    background: rgba(24, 24, 24, 1.0);
+    width: 250px;
+    overflow: hidden;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+
+  .title {
+    font-size: 12px;
+    color: white;
+    text-shadow: $accent-color 0px 0px 12px;
+  }
+
+  .message {
+    font-size: 12px;
+    color: #cbd1e3;
+  }
+</style>
